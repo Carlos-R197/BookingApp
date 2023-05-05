f74866c9eccb96d0fd367905c62f29d74e6b2841
Carlos Roque <carlosroque198@gmail.com>
2023-05-05 19:16:57 -0400

    Made appropiate changes to user and register to work with the api and created the the user context.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,38 @@
-import { Link } from "react-router-dom"
+import axios from "axios"
+import { useContext, useState } from "react"
+import { Link, Navigate } from "react-router-dom"
+import { UserContext } from "../context/UserContext"
 
 export default function LoginPage() {
+  const [ email, setEmail ] = useState("")
+  const [ password, setPassword] = useState("")
+  const [ redirect, setRedirect ] = useState(false)
+  const { setUser } = useContext(UserContext)
+
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault()
+    try {
+      const { data } = await axios.post("/login", { email: email, password: password })
+      setUser(data)
+      alert("Login successful")
+      setRedirect(true)
+    } catch (e) {
+      alert("Login failed")
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to="/"/>
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-32">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-lg">
-          <input type="email" placeholder="email@gmail.com" />
-          <input type="password" placeholder="your_password" />
-          <button className="primary">Login</button>
+        <form className="max-w-lg" onSubmit={handleLoginSubmit}>
+          <input type="email" placeholder="email@gmail.com" value={email} onChange={ev => setEmail(ev.target.value)}/>
+          <input type="password" placeholder="your_password" value={password} onChange={ev => setPassword(ev.target.value)}/>
+          <button className="primary" onClick={handleLoginSubmit}>Login</button>
           <div className="text-center text-gray-500">
             Don't have an account yet? <Link className="text-black underline" to={"/register"}>Register now</Link> 
           </div>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,19 +1,39 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios"
 
 export default function RegisterPage() {
   const [ username, setUsername ] = useState("")
   const [ email, setEmail ] = useState("")
   const [ password, setPassword ] = useState("")
 
+  async function registerUser(ev) {
+    ev.preventDefault()
+    try {
+      await axios.post("/register", {
+        username: username,
+        email: email,
+        password: password
+      })
+      alert("Registration successful. Now you can log in")
+    } catch (e) {
+      if (e.response.status == 422) {
+        alert("Email is alredy in usage")
+      }
+      else {
+        alert("Registration failed. Try again later")
+      }
+    }
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-32">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-lg">
-          <input type="text" placeholder="John Doe" value={username} />
-          <input type="email" placeholder="email@gmail.com" value={email} />
-          <input type="password" placeholder="your_password" value={password} />
+        <form className="max-w-lg" onSubmit={registerUser}>
+          <input type="text" placeholder="John Doe" value={username} onChange={ev => setUsername(ev.target.value)}/>
+          <input type="email" placeholder="email@gmail.com" value={email} onChange={ev => setEmail(ev.target.value)}/>
+          <input type="password" placeholder="your_password" value={password} onChange={ev => setPassword(ev.target.value)}/>
           <button className="primary">Finish registration</button>
           <div className="text-center text-gray-500">
             Don't have an account yet? <Link className="text-blue-500 underline" to={"/register"}>Register now</Link> 
