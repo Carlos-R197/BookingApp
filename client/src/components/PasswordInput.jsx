3191c16dd7ac23488f40522eeadde2c5f6bd75b8
Carlos Roque <carlosroque198@gmail.com>
2023-05-20 15:48:31 -0400

    Added show button for password inputs

--- client/src/components/PasswordInput.jsx
+++ client/src/components/PasswordInput.jsx
@@ -3,6 +3,7 @@ import isStrongPassword from "validator/lib/isStrongPassword"
 
 export default function PasswordInput({ value, setValue }) {
   const [ errorMessage, setErrorMessage ] = useState("")
+  const [ isShowingPassword, setIsShowingPassword ] = useState(false)
 
   function onInputBlur(value) {
     if (isStrongPassword(value)) {
@@ -12,17 +13,30 @@ export default function PasswordInput({ value, setValue }) {
     }
   }
 
+  function onShowPasswordClicked(ev) {
+    ev.preventDefault()
+    setIsShowingPassword(!isShowingPassword)
+  }
+
   return (
     <>
-      <input 
-        type="password" 
-        placeholder="Password#98" 
-        value={value} 
-        required
-        maxLength={50} 
-        onChange={ev => setValue(ev.target.value)}
-        onBlur={ev => onInputBlur(ev.target.value)}
+      <div className="relative">
+        <input 
+          type={isShowingPassword ? "text" : "password"} 
+          placeholder="Password#98" 
+          value={value} 
+          required
+          maxLength={50} 
+          onChange={ev => setValue(ev.target.value)}
+          onBlur={ev => onInputBlur(ev.target.value)} 
         />
+        <button className="absolute right-3 top-3 bg-white" onClick={onShowPasswordClicked}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+          </svg>
+        </button>
+      </div>
       <span className="text-red-500">{errorMessage}</span>
     </>
   )
